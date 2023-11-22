bdda7859d0b58edfa9682ff9d3160f309fb8f216
Oak Soe Htet <oaksoe.h@ku.th>
2023-11-23 00:07:36 +0700

    added Database and Table classes. added initialize, login and exit functions

--- project_manage.py
+++ project_manage.py
@@ -1,70 +1,79 @@
-# BEGIN part 1
-
 # import database module
+import csv
+from database import Table, Database
 
-# define a funcion called initializing
+database = None
+persons_table = None
+login_table = None
 
+
+# define a function called initializing
 def initializing():
-    pass
+    # create an object to read all csv files that will serve as a persistent state for this program
+    # create all the corresponding tables for those csv files
+    # see the guide how many tables are needed
+    # add all these tables to the database
+    global database, persons_table, login_table
+    database = Database()
+    persons_table = Table('persons', 'persons.csv')
+    login_table = Table('login', 'login.csv')
+    database.insert(persons_table)
+    database.insert(login_table)
 
-# here are things to do in this function:
-
-    # create an object to read an input csv file, persons.csv
-
-    # create a 'persons' table
-
-    # add the 'persons' table into the database
-
-    # create a 'login' table
-
-    # the 'login' table has the following keys (attributes):
-        # person_id
-        # username
-        # password
-        # role
-
-    # a person_id is the same as that in the 'persons' table
-
-    # let a username be a person's fisrt name followed by a dot and the first letter of that person's last name
-
-    # let a password be a random four digits string
-
-    # let the initial role of all the students be Member
-
-    # let the initial role of all the faculties be Faculty
-
-    # create a login table by performing a series of insert operations; each insert adds a dictionary to a list
-
-    # add the 'login' table into the database
 
 # define a funcion called login
-
 def login():
-    pass
+    # add code that performs a login task
+    # ask a user for a username and password
+    username = input('Username: ')
+    password = input('Password: ')
 
-# here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [person_id, role] if valid, otherwise returning None
+    # returns [ID, role] if valid, otherwise returning None
+    for user in database.search('login'):
+        if user['username'] == username and user['password'] == password:
+            return user['ID'], user['role']
+    return None
+
+
+# define a function called exit
+def exit():
+    with open('persons.csv', 'w', newline=''):
+        persons_writer = csv.DictWriter('persons.csv', ['ID', 'fist', 'last', 'type'])
+        persons_writer.writeheader()
+        persons_writer.writerows(persons_table.table)
+    with open('login.csv', 'w', newline=''):
+        login_writer = csv.DictWriter('login.csv', ['ID', 'username', 'password', 'role'])
+        login_writer.writeheader()
+        login_writer.writerows(login_table.table)
+
+    # write out all the tables that have been modified to the corresponding csv files
+    # By now, you know how to read in a csv file and transform it into a list of dictionaries.
+    # For this project, you also need to know how to do the reverse,
+    # i.e., writing out to a csv file given a list of dictionaries.
+    # See the link below for a tutorial on how to do this:
+    # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
+
 
 # make calls to the initializing and login functions defined above
 
 initializing()
 val = login()
 
-# END part 1
-
-# CONTINUE to part 2 (to be done for the next due date)
-
-# based on the return value for login, activate the code that performs activities according to the role defined for that person_id
+# based on the return value for login,
+# activate the code that performs activities according to the role defined for that person_id
 
 # if val[1] = 'admin':
-    # do admin related activities
-# elif val[1] = 'advisor':
-    # do advisor related activities
-# elif val[1] = 'lead':
-    # do lead related activities
+    # see and do admin related activities
+# elif val[1] = 'student':
+    # see and do student related activities
 # elif val[1] = 'member':
-    # do member related activities
+    # see and do member related activities
+# elif val[1] = 'lead':
+    # see and do lead related activities
 # elif val[1] = 'faculty':
-    # do faculty related activities
+    # see and do faculty related activities
+# elif val[1] = 'advisor':
+    # see and do advisor related activities
+
+# once everything is done, make a call to the exit function
+exit()
